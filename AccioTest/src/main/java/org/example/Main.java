b479fe20fe66839656c2b518956a5fe502a4bad5
shaik kasim <102864229+shaik123kasim@users.noreply.github.com>
2025-06-13 11:37:58 +0530

    Add files via upload

---
+++ AccioTest/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+        static class Product {
+            public int product(int x, int y) {
+                return x * y;
+            }
+            public int product(int x, int y, int z) {
+                return x * y * z;
+            }
+            public double product(double x, double y) {
+                return x * y;
+            }
+        }
+        public static void main(String[] args) {
+
+            Product p = new Product();
+            System.out.println(p.product(2, 3));
+            System.out.println(p.product(2, 3, 4));
+            System.out.println(p.product(2.5, 3.5));
+        }
+    }
