6ceb48a7fe43b835de274f46b910f66014d34367
shaik <shaikkashimbasha14@gmail.com>
2025-06-13 12:09:43 +0530

    assignment done

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,22 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-     
+    static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        System.out.println(p.product(2, 3));
+        System.out.println(p.product(2, 3, 4));
+        System.out.println(p.product(2.5, 3.5));
+    }
 }
