599a9b2e716dcee1df977fe6d4881a975e46264c
shaik <shaikkashimbasha14@gmail.com>
2025-06-14 09:02:16 +0530

    accio assignment

---
+++ src/main/java/com/driver/Main.java
@@ -0,0 +1,25 @@
+package com.driver;
+
+public class Main {
+    static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product p = new Product();
+
+
+        System.out.println( p.product(3, 4));
+        System.out.println( p.product(2, 3, 4));
+        System.out.println( p.product(2.5, 4.0));
+    }
+}
